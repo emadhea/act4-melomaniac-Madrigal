1c997501a20abd9e36ff4817393c31bf43f638c9
MadrigalEmaDhea <emamadrigal64@gmail.com>
2022-04-14 16:35:26 +0800

    second

---
+++ Song1.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity-index.php"> Rock My World </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity-Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Rock My World</h1>
+                <p class="lead">Medicine to your heart and soul</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>With A Smile</h2>";
+                        echo "<p class='lead'>Eraserheads</p>";
+
+                        echo "<p> Lift your head, baby, don't be scared</p>";
+                        echo "<p> Of the things that could go wrong along the way</p>";
+                        echo "<p> You'll get by with a smile</p>";
+                        echo "<p> You can't win at everything but you can try</p>";
+                        echo "<br/>";
+
+                        echo "<p> Baby, you don't have to worry</p>";
+                        echo "<p> 'Coz there ain't no need to hurry</p>";
+                        echo "<p> No one ever said that there's an easy way</p>";
+                        echo "<p> When they're closing all their doors</p>";
+                        echo "<p> And they don't want you anymore</p>";
+                        echo "<p> This sounds funny but I'll say it anyway</p>";
+                        echo "<br?>";
+
+                        echo "<p> Girl I'll stay through the bad times</p>";
+                        echo "<p> Even if I have to fetch you everyday</p>";
+                        echo "<p> We'll get by with a smile</p>";
+                        echo "<p> You can never be too happy in this life</p>";
+                        echo "<br/>";
+
+                        echo "<p> In a world where everybody</p>";
+                        echo "<p> Hates a happy ending story</p>";
+                        echo "<p> It's a wonder love can make the world go round</p>";
+                        echo "<p> But don't let it bring you down</p>";
+                        echo "<p> And turn your face into a frown</p>";
+                        echo "<p> You'll get along with a little prayer and a song</p>";
+                        echo "<br/>";
+
+                        echo "<p> (Too doo doo...)</p>";
+                        echo "<p> Let me hear you sing it</p>";
+                        echo "<p> (Too doo doo...)</p>";
+                        echo "<br/>";
+
+                        echo "<p> In a world where everybody</p>";
+                        echo "<p> Hates a happy ending story</p>";
+                        echo "<p> It's a wonder love can make the world go round</p>";
+                        echo "<p> But don't let it bring you down</p>";
+                        echo "<p> And turn your face into a frown</p>";
+                        echo "<p> You'll get along with a little prayer and a song</p>";
+                        echo "<br/>";
+
+                        echo "<p> Lift your head, baby, don't be scared</p>";
+                        echo "<p> Of the things that could go wrong along the way</p>";
+                        echo "<p> You'll get by with a smile</p>";
+                        echo "<p> Now it's time to kiss away those tears goodbye</p>";
+                        echo "<br/>";
+
+                        echo "<p> (Too doo doo...)</p>";
+                        echo "<p> Let me hear you sing it</p>";
+                        echo "<p> (Too doo doo...)</p>";
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ema Dhea S. Madrigal</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
