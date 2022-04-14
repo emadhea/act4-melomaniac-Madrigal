dac27373af00f2d1e1641ffcc6b4910b7da2fa20
MadrigalEmaDhea <emamadrigal64@gmail.com>
2022-04-14 17:49:32 +0800

    fourth

--- Song3.php
+++ Song3.php
@@ -44,105 +44,51 @@
 
                         <?php 
 
-                        echo "<h2 class='text-primary'>Complicated</h2>";
-                        echo "<p class='lead'>Avril Lavigne</p>";
+                        echo "<h2 class='text-primary'>Santeria</h2>";
+                        echo "<p class='lead'>Sublime</p>";
 
-                        echo "<p> Uh huh, life's like this</p>";
-                        echo "<p> Uh huh, uh huh, that's the way it is</p>";
-                        echo "<p> 'Cause life's like this</p>";
-                        echo "<p> Uh huh, uh huh that's the way it is</p>";
+                        echo "<p> I don't practice Santeria</p>";
+                        echo "<p> I ain't got no crystal ball</p>";
+                        echo "<p> Well, I had a million dollars but I, I'd spend it all.</p>";
+                        echo "<p> If I could find that Heina, and that Sancho that she's found.</p>";
+                        echo "<p> Well, I'd pop a cap in Sancho and I'd slap her down.</p>";
                         echo "<br/>";
 
-                        echo "<p> Chill out, what you yellin' for?</p>";
-                        echo "<p> Lay back, it's all been done before</p>";
-                        echo "<p> And if you could only let it be</p>";
-                        echo "<p> You would see</p>";
-                        echo "<p> I like you the way you are</p>";
-                        echo "<p> When we're drivin' in your car</p>";
-                        echo "<p> And you're talkin' to me one-on-one</p>";
-                        echo "<p> But you've become...</p>";
-                        echo "<br?>";
-
-                        echo "<p> Somebody else 'round everyone else</p>";
-                        echo "<p> You're watchin' your back like you can't relax</p>";
-                        echo "<p> You're tryin' to be cool</p>";
-                        echo "<p> You look like a fool to me</p>";
-                        echo "<p> Tell me</p>";
+                        echo "<p> What I really wanna know, mah baby, mmmm...</p>";
+                        echo "<p> What I really wanna say I can't define.</p>";
+                        echo "<p> Well, it's love, that I need. Oh...</p>";
                         echo "<br/>";
 
-                        echo "<p> Why'd you have to go and make things so complicated?</p>";
-                        echo "<p> I see the way you're actin' like you're somebody else</p>";
-                        echo "<p> Gets me frustrated</p>";
-                        echo "<p> Life's like this</p>";
-                        echo "<p> You, and you fall and you crawl and you break</p>";
-                        echo "<p> And you take what you get and you turn it into honesty</p>";
-                        echo "<p> You promised me I'm never gonna find you fake it</p>";
-                        echo "<p> No, no, no</p>";
+                        echo "<p> My soul will have to wait till I get back, to find a Heina of my own.</p>";
+                        echo "<p> Daddy's gonna love one and all.</p>";
+                        echo "<p> I feel the break, feel the break, feel the break and I gotta live it up.You're tryin' to be cool</p>";
+                        echo "<p> Oh, yeah, uh huh.</p>";
+                        echo "<p> Well, I swear that I.</p>";
                         echo "<br/>";
 
-                        echo "<p> You come over unannounced</p>";
-                        echo "<p> Dressed up like you're somethin' else</p>";
-                        echo "<p> Where you are and where it's at, you see</p>";
-                        echo "<p> You're makin' me</p>";
-                        echo "<p> Laugh out when you strike your pose</p>";
-                        echo "<p> Take off all your preppy clothes</p>";
-                        echo "<p> You know, you're not foolin' anyone</p>";
-                        echo "<p> When you become</p>";
+                        echo "<p> What I really wanna know, ah baby.</p>";
+                        echo "<p> What I really wanna say I can't define, got love make it go.</p>";
+                        echo "<p> My soul will have to...</p>";
                         echo "<br/>";
 
-                        echo "<p> Somebody else 'round everyone else</p>";
-                        echo "<p> You're watchin' your back like you can't relax</p>";
-                        echo "<p> You're tryin' to be cool</p>";
-                        echo "<p> You look like a fool to me</p>";
-                        echo "<p> Tell me</p>";
+                        echo "<p> [Instrumental break]</p>";
                         echo "<br/>";
 
-                        echo "<p> Why'd you have to go and make things so complicated?</p>";
-                        echo "<p> I see the way you're actin' like you're somebody else</p>";
-                        echo "<p> Gets me frustrated</p>";
-                        echo "<p> Life's like this</p>";
-                        echo "<p> You, and you fall and you crawl and you break</p>";
-                        echo "<p> And you take what you get and you turn it into honesty</p>";
-                        echo "<p> You promised me I'm never gonna find you fake it</p>";
-                        echo "<p> No, no, no</p>";
+                        echo "<p> Oh...</p>";
+                        echo "<p> What I really wanna say, mah baby.</p>";
+                        echo "<p> What I really wanna say is I've got mine, and I'll make it, oh yes I'm coming up.</p>";
                         echo "<br/>";
 
-                        echo "<p> No, no, no</p>";
-                        echo "<p> No, no, no</p>";
-                        echo "<p> No, no, no</p>";
-
-                        echo "<p> Chill out, what you yellin' for?</p>";
-                        echo "<p> Lay back, it's all been done before</p>";
-                        echo "<p> And if you could only let it be</p>";
-                        echo "<p> You will see...</p>";
-
-                        echo "<p> Somebody else 'round everyone else</p>";
-                        echo "<p> You're watchin' your back like you can't relax</p>";
-                        echo "<p> You're tryin' to be cool</p>";
-                        echo "<p> You look like a fool to me</p>";
-                        echo "<p> Tell me</p>";
+                        echo "<p> Tell Sanchito that if he knows what is good for him he best go run and hide.</p>";
+                        echo "<p> Daddy's got a new Forty-Five.</p>";
+                        echo "<p> And I won't think twice to stick that barrel straight down Sancho's throat.</p>";
+                        echo "<p> Believe me when I say that I got somethin' for his punk ass.</p>";
                         echo "<br/>";
 
-                        echo "<p> Why'd you have to go and make things so complicated?</p>";
-                        echo "<p> I see the way you're actin' like you're somebody else</p>";
-                        echo "<p> Gets me frustrated</p>";
-                        echo "<p> Life's like this</p>";
-                        echo "<p> You, and you fall and you crawl and you break</p>";
-                        echo "<p> And you take what you get and you turn it into honesty</p>";
-                        echo "<p> You promised me I'm never gonna find you fake it</p>";
-                        echo "<p> No, no</p>";
+                        echo "<p> What I really wanna know, mah baby.</p>";
+                        echo "<p> Oh, what I really wanna say is there's just one, way back, and I'll make it, yeah.</p>";
+                        echo "<p> My soul will have to wait, yeah, yeah, yeah.</p>";
                         echo "<br/>";
-
-                        echo "<p> Why'd you have to go and make things so complicated? (yeah, yeah)</p>";
-                        echo "<p> I see the way you're actin' like you're somebody else</p>";
-                        echo "<p> Gets me frustrated</p>";
-                        echo "<p> Life's like this</p>";
-                        echo "<p> You, and you fall and you crawl and you break</p>";
-                        echo "<p> And you take what you get and you turn it into honesty</p>";
-                        echo "<p> You promised me I'm never gonna find you fake it</p>";
-                        echo "<p> No, no, no</p>";
-                        echo "<br/>";
-
 
                         ?>
 
