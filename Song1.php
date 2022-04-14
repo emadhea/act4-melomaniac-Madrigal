1d8f2fb9a68ebca4f433af94d5c0c6c317c05c7f
MadrigalEmaDhea <emamadrigal64@gmail.com>
2022-04-14 17:36:20 +0800

    third

--- Song1.php
+++ Song1.php
@@ -14,15 +14,15 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="https://localhost/melomaniac-activity-index.php"> Rock My World </a>
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php"> Rock My World </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity-Song1.php">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
                     </ul>
                 </div>
             </div>
@@ -40,6 +40,7 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
+
 
                         <?php 
 
---
+++ Song2.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php"> Rock My World </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Rock My World</h1>
+                <p class="lead">Medicine to your heart and soul</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>Complicated</h2>";
+                        echo "<p class='lead'>Avril Lavigne</p>";
+
+                        echo "<p> Uh huh, life's like this</p>";
+                        echo "<p> Uh huh, uh huh, that's the way it is</p>";
+                        echo "<p> 'Cause life's like this</p>";
+                        echo "<p> Uh huh, uh huh that's the way it is</p>";
+                        echo "<br/>";
+
+                        echo "<p> Chill out, what you yellin' for?</p>";
+                        echo "<p> Lay back, it's all been done before</p>";
+                        echo "<p> And if you could only let it be</p>";
+                        echo "<p> You would see</p>";
+                        echo "<p> I like you the way you are</p>";
+                        echo "<p> When we're drivin' in your car</p>";
+                        echo "<p> And you're talkin' to me one-on-one</p>";
+                        echo "<p> But you've become...</p>";
+                        echo "<br?>";
+
+                        echo "<p> Somebody else 'round everyone else</p>";
+                        echo "<p> You're watchin' your back like you can't relax</p>";
+                        echo "<p> You're tryin' to be cool</p>";
+                        echo "<p> You look like a fool to me</p>";
+                        echo "<p> Tell me</p>";
+                        echo "<br/>";
+
+                        echo "<p> Why'd you have to go and make things so complicated?</p>";
+                        echo "<p> I see the way you're actin' like you're somebody else</p>";
+                        echo "<p> Gets me frustrated</p>";
+                        echo "<p> Life's like this</p>";
+                        echo "<p> You, and you fall and you crawl and you break</p>";
+                        echo "<p> And you take what you get and you turn it into honesty</p>";
+                        echo "<p> You promised me I'm never gonna find you fake it</p>";
+                        echo "<p> No, no, no</p>";
+                        echo "<br/>";
+
+                        echo "<p> You come over unannounced</p>";
+                        echo "<p> Dressed up like you're somethin' else</p>";
+                        echo "<p> Where you are and where it's at, you see</p>";
+                        echo "<p> You're makin' me</p>";
+                        echo "<p> Laugh out when you strike your pose</p>";
+                        echo "<p> Take off all your preppy clothes</p>";
+                        echo "<p> You know, you're not foolin' anyone</p>";
+                        echo "<p> When you become</p>";
+                        echo "<br/>";
+
+                        echo "<p> Somebody else 'round everyone else</p>";
+                        echo "<p> You're watchin' your back like you can't relax</p>";
+                        echo "<p> You're tryin' to be cool</p>";
+                        echo "<p> You look like a fool to me</p>";
+                        echo "<p> Tell me</p>";
+                        echo "<br/>";
+
+                        echo "<p> Why'd you have to go and make things so complicated?</p>";
+                        echo "<p> I see the way you're actin' like you're somebody else</p>";
+                        echo "<p> Gets me frustrated</p>";
+                        echo "<p> Life's like this</p>";
+                        echo "<p> You, and you fall and you crawl and you break</p>";
+                        echo "<p> And you take what you get and you turn it into honesty</p>";
+                        echo "<p> You promised me I'm never gonna find you fake it</p>";
+                        echo "<p> No, no, no</p>";
+                        echo "<br/>";
+
+                        echo "<p> No, no, no</p>";
+                        echo "<p> No, no, no</p>";
+                        echo "<p> No, no, no</p>";
+
+                        echo "<p> Chill out, what you yellin' for?</p>";
+                        echo "<p> Lay back, it's all been done before</p>";
+                        echo "<p> And if you could only let it be</p>";
+                        echo "<p> You will see...</p>";
+
+                        echo "<p> Somebody else 'round everyone else</p>";
+                        echo "<p> You're watchin' your back like you can't relax</p>";
+                        echo "<p> You're tryin' to be cool</p>";
+                        echo "<p> You look like a fool to me</p>";
+                        echo "<p> Tell me</p>";
+                        echo "<br/>";
+
+                        echo "<p> Why'd you have to go and make things so complicated?</p>";
+                        echo "<p> I see the way you're actin' like you're somebody else</p>";
+                        echo "<p> Gets me frustrated</p>";
+                        echo "<p> Life's like this</p>";
+                        echo "<p> You, and you fall and you crawl and you break</p>";
+                        echo "<p> And you take what you get and you turn it into honesty</p>";
+                        echo "<p> You promised me I'm never gonna find you fake it</p>";
+                        echo "<p> No, no</p>";
+                        echo "<br/>";
+
+                        echo "<p> Why'd you have to go and make things so complicated? (yeah, yeah)</p>";
+                        echo "<p> I see the way you're actin' like you're somebody else</p>";
+                        echo "<p> Gets me frustrated</p>";
+                        echo "<p> Life's like this</p>";
+                        echo "<p> You, and you fall and you crawl and you break</p>";
+                        echo "<p> And you take what you get and you turn it into honesty</p>";
+                        echo "<p> You promised me I'm never gonna find you fake it</p>";
+                        echo "<p> No, no, no</p>";
+                        echo "<br/>";
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ema Dhea S. Madrigal</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
