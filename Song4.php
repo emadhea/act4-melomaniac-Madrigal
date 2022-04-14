2acdae96e9e66f77b7d590d21464efec502f33fd
MadrigalEmaDhea <emamadrigal64@gmail.com>
2022-04-14 17:56:55 +0800

    fifth

---
+++ Song4.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php"> Rock My World </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Rock My World</h1>
+                <p class="lead">Medicine to your heart and soul</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>Cancer</h2>";
+                        echo "<p class='lead'>My Chemical Romance</p>";
+
+                        echo "<p> Turn away, if you could get me a drink</p>";
+                        echo "<p> Of water, 'cause my lips are chapped and faded</p>";
+                        echo "<p> Call my Aunt Marie, help her gather all my things</p>";
+                        echo "<p> And bury me in all my favorite colors</p>";
+                        echo "<p> My sisters and my brothers, still, I will not kiss you</p>";
+                        echo "<p> 'Cause the hardest part of this is leaving you</p>";
+                        echo "<br/>";
+
+                        echo "<p> Now turn away, 'cause I'm awful just to see</p>";
+                        echo "<p> 'Cause all my hair's abandoned all my body</p>";
+                        echo "<p> Oh, my agony, know that I will never marry</p>";
+                        echo "<p> Baby, I'm just soggy from the chemo</p>";
+                        echo "<p> But counting down the days to go, it just ain't living</p>";
+                        echo "<p> And I just hope you know</p>";
+                        echo "<br/>";
+
+                        echo "<p> That if you say (If you say...) goodbye today (Goodbye today...)</p>";
+                        echo "<p> I'd ask you to be true (I'd ask you to be true...)</p>";
+                        echo "<p> 'Cause the hardest part of this is leaving you</p>";
+                        echo "<p> 'Cause the hardest part of this is leaving you</p>";
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ema Dhea S. Madrigal</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
