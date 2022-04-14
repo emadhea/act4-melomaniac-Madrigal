e3689d62c4819ba5fbf64c55b18434e213ac637b
MadrigalEmaDhea <emamadrigal64@gmail.com>
2022-04-14 15:46:35 +0800

    first

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity-index.php"> Rock My World </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity-Song1.php">Song 1</a></li>
                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
@@ -30,8 +30,8 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <h1 class="fw-bolder">Welcome to Rock My World</h1>
+                <p class="lead">Medicine to your heart and soul</p>
                 
             </div>
         </header>
@@ -40,16 +40,106 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>It's You</h2>";
+                        echo "<p class='lead'>Ali Gatie</p>";
+
+                        echo "<p> It's you, it's always you.</p>";
+                        echo "<p> If I'm ever gonna fall in love I know it's gon' be you.</p>";
+                        echo "<p> It's you, it's always you.</p>";
+                        echo "<p> Met a lot of people, but nobody feels like you.</p>";
+                        echo "<br/>";
+
+                        echo "<p> So, please, don't break my heart.</p>";
+                        echo "<p> Don't tear me apart.</p>";
+                        echo "<p> I know how it starts.</p>";
+                        echo "<p> Trust me: I've been broken before.</p>";
+                        echo "<p> Don't break me again.</p>";
+                        echo "<p> I am delicate.</p>";
+                        echo "<p> Please, Don't break my heart.</p>";
+                        echo "<p> Trust me: I've been broken before.</p>";
+                        echo "<br?>";
+
+                        echo "<p> I've been broken.</p>";
+                        echo "<p> Yeah, I know how it feels.</p>";
+                        echo "<p> To be open</p>";
+                        echo "<p> And then find out your love isn't real.</p>";
+                        echo "<p> I'm still hurting.</p>";
+                        echo "<p> Yeah. I'm hurting inside.</p>";
+                        echo "<p> I'm so scared to fall in love,</p>";
+                        echo "<p> But if it's you then I'll try.</p>";
+                        echo "<br/>";
+
+                        echo "<p> It's you, it's always you.</p>";
+                        echo "<p> If I'm ever gonna fall in love I know it's gon' be you.</p>";
+                        echo "<p> It's you, it's always you.</p>";
+                        echo "<p> Met a lot of people, but nobody feels like you.</p>";
+                        echo "<br/>";
+
+                        echo "<p> So, please, don't break my heart.</p>";
+                        echo "<p> Don't tear me apart.</p>";
+                        echo "<p> I know how it starts.</p>";
+                        echo "<p> Trust me: I've been broken before.</p>";
+                        echo "<p> Don't break me again.</p>";
+                        echo "<p> I am delicate.</p>";
+                        echo "<p> Please, don't break my heart.</p>";
+                        echo "<p> Trust me: I've been broken before.</p>";
+                        echo "<br/>";
+
+                        echo "<p> I know I'm not the best at choosing lovers.</p>";
+                        echo "<p> We both know my past speaks for itself.</p>";
+                        echo "<p> For itself.</p>";
+                        echo "<p> If you don't think that we're right for each other</p>";
+                        echo "<p> (Baby, no.)</p>";
+                        echo "<p> Then, please, don't let history repeat itself.</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause I want you. Yeah. I want you. Yeah.</p>";
+                        echo "<p> There's nothing else I want.</p>";
+                        echo "<p> 'Cause I want you. Yeah. I want you. Yeah.</p>";
+                        echo "<p> And you're the only thing I want.</p>";
+                        echo "<br/>";
+
+                        echo "<p> It's you, it's always you.</p>";
+                        echo "<p> If I'm ever gonna fall in love I know it's gon' be you.</p>";
+                        echo "<p> It's you, it's always you.</p>";
+                        echo "<p> Met a lot of people, but nobody feels like you.</p>";
+                        echo "<br/>";
+
+                        echo "<p> So, please, don't break my heart.</p>";
+                        echo "<p> Don't tear me apart.</p>";
+                        echo "<p> I know how it starts. </p>";
+                        echo "<p> Trust me: I've been broken before.</p>";
+                        echo "<p> Don't break me again.</p>";
+                        echo "<p> I am delicate.</p>";
+                        echo "<p> Please, don't break my heart.</p>";
+                        echo "<p> Trust me: I've been broken before.</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause I want you, baby. I want you.</p>";
+                        echo "<p> Baby, I want you, baby. I want you.</p>";
+                        echo "<p> Baby, I want you, baby. I want you.</p>";
+                        echo "<p> Baby, I want you, you, you.</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause I want you, baby. I want you.</p>";
+                        echo "<p> Baby, I want you, baby. I want you.</p>";
+                        echo "<p> Baby, I want you, baby. I want you.</p>";
+                        echo "<p> Baby, I want you, you, you.</p>";
+                        echo "<br/>";
+
+                        ?>
+
+
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ema Dhea S. Madrigal</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
