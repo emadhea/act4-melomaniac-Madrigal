2f3d3c1a1a1fda0fd3c5c71b130fa4e2d8c66090
MadrigalEmaDhea <emamadrigal64@gmail.com>
2022-04-14 18:06:51 +0800

    sixth

---
+++ Song5.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php"> Rock My World </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Rock My World</h1>
+                <p class="lead">Medicine to your heart and soul</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>My Heart</h2>";
+                        echo "<p class='lead'>Paramore</p>";
+
+                        echo "<p> I am finding out that maybe I was wrong</p>";
+                        echo "<p> That I've fallen down and I can't do this alone</p>";
+                        echo "<br/>";
+
+                        echo "<p> Stay with me, this is what I need, please?</p>";
+                        echo "<br/>";
+
+                        echo "<p> Sing us a song and we'll sing it back to you</p>";
+                        echo "<p> We could sing our own but what would it be without you?</p>";
+                        echo "<br/>";
+
+                        echo "<p> I am nothing now and it's been so long</p>";
+                        echo "<p> Since I've heard the sound, the sound of my only hope</p>";
+                        echo "<br/>";
+
+                        echo "<p> This time I will be listening.</p>";
+                        echo "<br/>";
+
+                        echo "<p> Sing us a song and we'll sing it back to you</p>";
+                        echo "<p> We could sing our own but what would it be without you?</p>";
+                        echo "<br/>";
+
+                        echo "<p> This heart, it beats, beats for only you</p>";
+                        echo "<p> This heart, it beats, beats for only you</p>";
+                        echo "<br/>";
+
+                        echo "<p> This heart, it beats, beats for only you</p>";
+                        echo "<p> My heart is yours</p>";
+                        echo "<br/>";
+
+                        echo "<p> This heart, it beats, beats for only you</p>";
+                        echo "<p> My heart is yours</p>";
+                        echo "<p> (My heart, it beats for you)</p>";
+                        echo "<br/>";
+
+                        echo "<p> This heart, it beats, beats for only you (It beats, beats for only you)</p>";
+                        echo "<p> My heart is yours (My heart is yours)</p>";
+                        echo "<br/>";
+
+                        echo "<p> This heart, it beats, beats for only you (Please don't go now, please don't fade away)</p>";
+                        echo "<p> My heart, my heart is yours (Please don't go now, please don't fade away)</p>";
+                        echo "<br/>";
+
+                        echo "<p> (Please don't go now, please don't fade away) My heart is yours</p>";
+                        echo "<p> (Please don't go now, please don't fade away) My heart is yours</p>";
+                        echo "<p> (Please don't go, please don't fade away)</p>";
+                        echo "<p> (Please don't go now, please don't fade away) My heart is...</p>";
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ema Dhea S. Madrigal</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
